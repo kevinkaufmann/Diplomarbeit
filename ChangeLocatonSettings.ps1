562fd6442640c0c32428218d27ede276c526acc9
kevinkaufmann <57481748+kevinkaufmann@users.noreply.github.com>
2020-10-02 22:39:32 +0200

    Add files via upload

---
+++ ChangeLocatonSettings.ps1
@@ -0,0 +1,6 @@
+﻿Set-WinHomeLocation 223 
+Set-WinUserLanguageList -LanguageList de-CH -Force
+Set-WinSystemLocale -SystemLocale de-DE
+Set-Culture -CultureInfo de-CH
+Set-TimeZone -Id "W. Europe Standard Time" -PassThru
+Set-Culture -CultureInfo de-CH
