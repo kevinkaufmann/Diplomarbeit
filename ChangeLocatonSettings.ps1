93d38e1cdbc80918dcaa131859de22b7ae4e2f5a
kevinkaufmann <57481748+kevinkaufmann@users.noreply.github.com>
2020-10-02 22:39:18 +0200

    Delete ChangeLocatonSettings.ps1

--- ChangeLocatonSettings.ps1
+++
@@ -1,8 +0,0 @@
-﻿Set-WinHomeLocation 223 
-Set-WinUserLanguageList -LanguageList de-CH -Force
-Set-WinSystemLocale -SystemLocale de-DE -Force
-Set-Culture -CultureInfo de-CH
-Set-TimeZone -Id "W. Europe Standard Time" -PassThru
-Set-Culture -CultureInfo de-CH
-DISM /online /Get-Intl
-
